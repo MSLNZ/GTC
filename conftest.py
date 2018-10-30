7838c29d79ce25ba8b0382f55f2316563f841d6c
jborbely <joe.borbely@gmail.com>
2018-10-30 17:26:09 +1300

    fix "SyntaxError: import * only allowed at module level" in Python 3

--- conftest.py
+++ conftest.py
@@ -3,11 +3,12 @@ Injects GTC in to the doctest namespace for pytest.
 """
 import pytest
 
+from GTC import *
+
 
 @pytest.fixture(scope='session', autouse=True)
 def add_gtc(doctest_namespace):
-    from GTC import *
-    for key, value in locals().items():
+    for key, value in globals().items():
         if key.startswith('_'):
             continue
         doctest_namespace[key] = value
