56ef6f0a5eb942c476411caed8d25dbdb44dbf87
jborbely <joe.borbely@gmail.com>
2019-05-30 17:17:15 +1200

    bump v1.1.0 -> v1.1.1.dev0

--- GTC/__init__.py
+++ GTC/__init__.py
@@ -92,7 +92,7 @@ __all__ = (
 )
  
 #----------------------------------------------------------------------------
-version = "1.1.0"
+version = "1.1.1.dev0"
 copyright = """Copyright (c) 2019, \
 Measurement Standards Laboratory of New Zealand"""
 
