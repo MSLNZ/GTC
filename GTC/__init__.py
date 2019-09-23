b13aaaef9f8f010e91383d99a8eaafe876e55c92
jborbely <joe.borbely@gmail.com>
2019-09-23 12:46:06 +1200

    change the version format so that the UserWarning goes away

--- GTC/__init__.py
+++ GTC/__init__.py
@@ -92,7 +92,7 @@ __all__ = (
 )
 
 #----------------------------------------------------------------------------
-version = "1.2.0.develop"
+version = "1.2.0.dev0"
 copyright = """Copyright (c) 2019, \
 Measurement Standards Laboratory of New Zealand"""
 
