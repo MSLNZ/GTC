b3f8e02616f756dada8dab51ae715162dc8b9511
Blair Hall <blairdhall@gmail.com>
2020-12-26 17:44:03 +1300

    _realft implemented

--- test/test_ft.py
+++ test/test_ft.py
@@ -49,34 +49,125 @@ class TestFFT(unittest.TestCase):
     def test_basis(self):
  
         N = 16
-        d = numpy.zeros(N,dtype='cfloat')
+        d = numpy.empty(N,dtype='cfloat')
 
         for k in xrange(N):
             
-            for i in range(N):
+            for i in xrange(N):
                 d[i] = complex( math.cos(2*math.pi*i*k/N),-math.sin(2*math.pi*i*k/N) )
             d = ft.fft(d)
             self.assertTrue( equivalent_complex(d[k],N) )
 
             # Reverse transform undoes FT 
             d = ft.ifft(d)
-            for i in range(N):
+            for i in xrange(N):
                 d_i = complex( math.cos(2*math.pi*i*k/N), -math.sin(2*math.pi*i*k/N) )
                 self.assertTrue( equivalent_complex(d[i],d_i) )
 
             # Check with numpy implementation. Note that numpy adopts the 
             # engineering form, so the sign of the exponent is opposite
-            for i in range(N):
+            for i in xrange(N):
                 d[i] = complex( math.cos(2*math.pi*i*k/N),math.sin(2*math.pi*i*k/N) )
             d = numpy.fft.fft(d)
             self.assertTrue( equivalent_complex(d[k],N) )
             
             # Reverse transform undoes FT 
             d = numpy.fft.ifft(d)
-            for i in range(N):
+            for i in xrange(N):
                 d_i = complex( math.cos(2*math.pi*i*k/N), math.sin(2*math.pi*i*k/N) )
                 self.assertTrue( equivalent_complex(d[i],d_i) )
+  
+#-----------------------------------------------------
+class Test_FFT(unittest.TestCase):
+    
+    """
+    Test the _fft routine. This is intended only 
+    for internal use. It uses arrays of float rather 
+    than complex. It remains to be seen whether this 
+    has any numerical advantage in some applications. 
+    """
+    
+    def test_dc(self):
+  
+        N = 32
+        
+        d = numpy.ones(N,dtype=float)        
+        d = ft._fft(d)
+        self.assertTrue( equivalent(d[0],N/2 ) )
+
+        d = numpy.ones(N>>1,dtype=float)        
+        d = numpy.fft.fft(d)
+        self.assertTrue( equivalent_complex(d[0],N/2) )
+        
+    def test_basis(self):
+ 
+        n = 32
+        N = n >> 1
+        d = numpy.empty(n,dtype='float')
+
+        for k in xrange(N):
             
+            for i in xrange(N):
+                d[2*i] = math.cos(2*math.pi*i*k/N)
+                d[2*i+1] = -math.sin(2*math.pi*i*k/N)    
+            d = ft._fft(d)
+            
+            self.assertTrue( equivalent(d[2*k],N) )
+            self.assertTrue( equivalent(d[2*k+1],0.0) )
+
+            # Reverse transform undoes FT 
+            d = ft._fft(d,True)
+            for i in xrange(N):
+                dr = math.cos(2*math.pi*i*k/N) * N
+                di = -math.sin(2*math.pi*i*k/N) * N   
+                self.assertTrue( equivalent(d[2*i],dr) )
+                self.assertTrue( equivalent(d[2*i+1],di) )
+
+#-----------------------------------------------------
+class Test_REALFT(unittest.TestCase):
+    
+    """
+    Test the _realft routine. This is intended only 
+    for internal use. It uses arrays of float rather 
+    than complex.  
+    """
+    
+    def test_dc(self):
+  
+        N = 16
+        
+        d = numpy.ones(N,dtype=float)        
+        d = ft._realft(d)
+        self.assertTrue( equivalent(d[0],N ) )
+
+        d = numpy.ones(N,dtype=float)        
+        d = numpy.fft.rfft(d)
+        self.assertTrue( equivalent_complex(d[0],N) )
+        
+    def test_basis(self):
+ 
+        N = 16
+        d = numpy.empty(N,dtype='float')
+
+        for k in xrange(1,N<<1):
+            
+            for i in xrange(N):
+                d[i] = math.cos(2*math.pi*i*k/N)
+                
+            d = ft._realft(d)
+                        
+            if 2*k < N:
+                self.assertTrue( equivalent(d[2*k],N/2) )
+                self.assertTrue( equivalent(d[2*k+1],0.0) )
+            elif 2*k == N:
+                self.assertTrue( equivalent(d[1],N) )
+                
+            # Reverse transform undoes the forward transform 
+            d = ft._realft(d,True)
+            for i in range(N):
+                dr = math.cos(2*math.pi*i*k/N) 
+                self.assertTrue( equivalent(d[i]*2/N,dr) )
+                
 #=====================================================
 if(__name__== '__main__'):
 
