9d8aea26af8d3d76106b4ebf5cb879bd29654e04
Blair Hall <blairdhall@gmail.com>
2024-01-15 08:48:48 +1300

    Syntax error fixed

--- setup.py
+++ setup.py
@@ -147,4 +147,4 @@ setup(
     extras_require={'tests': tests_require, 'docs': docs_require},
     cmdclass={'docs': BuildDocs, 'apidocs': ApiDocs},
     packages=find_packages(include=('GTC*',)),
-    data_files = [('GTC/schemas',['GTC/schemas/gtc_v_1_3_5.json','GTC/schemas/gtc_v_1_4_2.json']]
+    data_files = [('GTC/schemas',['GTC/schemas/gtc_v_1_3_5.json','GTC/schemas/gtc_v_1_4_2.json'])]
