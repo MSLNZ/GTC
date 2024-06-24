26a3561a0610da0519728e5f8a52ce074006c6e8
jborbely <joe.borbely@gmail.com>
2024-06-24 14:32:47 +1200

    bump to version 1.5.2.dev0

--- GTC/__init__.py
+++ GTC/__init__.py
@@ -113,7 +113,7 @@ if sys.version_info[:2] < (3, 8):
     )
     del warnings
 #----------------------------------------------------------------------------
-version = "1.5.1"
+version = "1.5.2.dev0"
 
 copyright = """Copyright (c) 2024, \
 Measurement Standards Laboratory of New Zealand"""
