22a8d4cb172da0800f6650e762cf18f7e16756b0
Blair Hall <blairdhall@gmail.com>
2024-07-21 16:40:30 +1200

    This version has resolved earlier confusion. Dependence on SVD and Cholesky modules is not needed, because numpy routines are available. The gls routine now constructs the CV matrix if one is not supplied. float64 set explicitly, or object. Docstrings improved

--- GTC/type_b_linear_models.py
+++ GTC/type_b_linear_models.py
@@ -3,58 +3,55 @@
 Module contents
 ---------------
   
+.. versionadded:: 2.0
+
 """
 import math
 import numpy as np
 
 from GTC.lib import value
 from GTC.type_a_linear_models import ModelFit
-# from GTC import SVD
-# from GTC import cholesky 
-
-from GTC.misc import _dtype_float
-
-# #----------------------------------------------------------------------------
-# def lsfit(x,y,sig=None,fn=None):
-    # """
-    # Solve `x @ beta = y` for `beta` 
-
-    # .. versionadded:: 1.4.x
-    
-    # :arg x: an array of stimulus data
-    # :arg y: a 1-D array of response data
-    # :arg sig: a 1-D array of standard deviations associated with response data
-    # :arg fn: evaluate a sequence of basis function values at a stimulus value
-    # :returns: `coef`, `res`, `ssr`
-    
-    # """    
-    # coef, res, ssr, _, _ = SVD.svdfit(x,y,sig,fn)
-    
-    # return coef, res, ssr  
   
+__all__ = (
+    'ols',
+    'wls',
+    'gls'
+)
 #----------------------------------------------------------------------------
 def lsfit(x,y,sig=None,fn=None):
     """
     Solve `x @ beta = y` for `beta` 
-
-    .. versionadded:: 1.4.x
     
     :arg x: an array of stimulus data
     :arg y: a 1-D array of response data
     :arg sig: a 1-D array of standard deviations associated with response data
-    :arg fn: evaluate a sequence of basis function values at a stimulus value
+    :arg fn: returns a sequence of basis function values at a stimulus value
     :returns: `coef`, `res`, `ssr`
+ 
+    A Singular Value Decomposition (SVD) algorithm is used to obtain the solution.
     
+    The returned `coef` is an uncertain-number sequence of parameter estimates for `beta`.
+        
+    The returned `ssr` is the squared sum of residuals of predicted values to the `y` data.
+    
+    If `x` or `y` are arrays of uncertain numbers, only the values are used in calculations.
+
+    When `sig` is not provided, the standard deviations are taken as unity
+    
+    When `fn` is provided, the routine will apply `fn` to each element 
+    in `x`, which must be a 1-D array, to obtain the rows of the objective matrix. 
+    When `fn` is `None`, `x` is taken as the objective matrix.
+ 
     """    
     # M - number of data points 
     # P - number of parameters to fit 
     M = len(x) 
     if sig is None: 
-        sig = np.ones( (len(x),) )
+        sig = np.ones( (M,), dtype=np.float64 )
     else:
         sig = np.array( sig, dtype=np.float64 )
         
-    # Allow uncertain-number inputs:
+    # Allow uncertain-number inputs, but
     # construct arrays `a` and `b` from values 
     if fn is None:
         P = len( x[0] )   
@@ -72,14 +69,14 @@ def lsfit(x,y,sig=None,fn=None):
         # each basis function at the stimulus point `x_i`
         P = len( fn(x[0])  )   
 
-        a = np.empty( (M,P), dtype=_dtype_float(x) )
+        a = np.empty( (M,P), dtype=np.float64 )
         b = np.empty( (M,), dtype=object )    
         
         for i in range(M):
             afunc_i = fn(x[i])
             tmp = 1.0/sig[i]
             for j in range(P):
-                a[i,j] = tmp*afunc_i[j]
+                a[i,j] = value( tmp*afunc_i[j] )
                 
             b[i] = tmp*y[i] 
             
@@ -89,10 +86,6 @@ def lsfit(x,y,sig=None,fn=None):
     u,w,vh = np.linalg.svd(a, full_matrices=False )
     v = vh.T    
     
-    # `TOL` is used to set relatively small singular values to zero
-    # Doing so avoids numerical precision problems, but will make the 
-    # solution slightly less accurate. The value can be varied.
-    TOL = 1E-5
     # Select almost singular values
     wmax = max(w)
     # wmin = min(w)
@@ -104,41 +97,44 @@ def lsfit(x,y,sig=None,fn=None):
     # # C is the condition number: the ratio of the largest to smallest 
     # # singular value in the SVD
     
+    # `TOL` is used to adjust the small singular values set to zero
+    # This avoids numerical precision problems, but will make the 
+    # solution slightly less accurate. 
+    TOL = 1E-5
+    
     thresh = TOL*wmax 
     w = np.array([ 
         w_i if w_i >= thresh else 0. 
             for w_i in w 
     ])
     
-    # coef = svbksb(u,w,v,b)
     coef = v @ np.diag(1 / w) @ u.T @ b
     
-    # cv_coef = svdvar(v,w) 
-    # wti = [
-        # 1.0/(w_i*w_i) if w_i != 0 else 0.0
-            # for w_i in w 
-    # ]
-    # cv_coef = v @ np.diag(wti) @ vh
-    # print(cv_coef)
-    # print([c.v for c in coef])
-
-# Residuals and sum of squared residuals
+    # Residuals and sum of squared residuals
     res = b - np.dot(a, coef) 
     ssr = math.fsum( value(res_i)**2 for res_i in res )
-          
+
     return coef, res, ssr  
   
 #----------------------------------------------------------------------------
 def ols(x,y,fn=None):
     """Ordinary least squares fit of ``y`` to ``x``
     
-    :arg x: sequence of stimulus values (independent-variable)
-    :arg y: sequence of response data (dependent-variable)   
-    :arg fn: a user-defined function relating the stimulus to the response
+    :arg x: an array of stimulus data (independent-variable)
+    :arg y: a 1-D array of response data (dependent-variable)
+    :arg fn: returns a sequence of basis function values at a stimulus value
     :arg label: suffix to label the fitted parameters
     :returns:   an object containing regression results
-    :rtype:     :class:`OLSFit``
+    :rtype:     :class:`OLSModel`
+
+    The argument `x` may be an array of numbers or uncertain numbers. 
     
+    The argument `y` must be an array of uncertain numbers.
+
+    When `fn` is provided, the routine will apply `fn` to each element 
+    in `x`, which must be a 1-D array, to obtain the rows of the objective matrix. 
+    When `fn` is `None`, `x` is taken as the objective matrix.
+
     """
     M = len(y)
     
@@ -156,13 +152,24 @@ def ols(x,y,fn=None):
 def wls(x,y,u_y=None,fn=None):    
     """Weighted least squares fit of ``y`` to ``x``
     
-    :arg x: a sequence of ``M`` stimulus values (independent-variables)
-    :arg y: a sequence of ``M`` responses (dependent-variable)  
-    :arg u_y: a sequence of ``M`` standard uncertainties in the responses
-    :arg fn: a user-defined function relating ``x`` the response
+    :arg x: an array of stimulus data (independent-variable)
+    :arg y: a 1-D array of response data (dependent-variable)
+    :arg u_y: sequence of standard uncertainties for response data    
+    :arg fn: returns a sequence of basis function values at a stimulus value
     :returns:   an object containing regression results
-    :rtype:     :class:`WLSFit``
+    :rtype:     :class:`WLSModel`
+ 
+    The argument `x` may be an array of numbers or uncertain numbers. 
     
+    The argument `y` must be an array of uncertain numbers.
+
+    When the optional argument `u_y` is not given, the uncertainties of the
+    `y` elements are used for provide weights for the calculation.
+    
+    When `fn` is provided, the routine will apply `fn` to each element 
+    in `x`, which must be a 1-D array, to obtain the rows of the objective matrix. 
+    When `fn` is `None`, `x` is taken as the objective matrix.
+ 
     """
     M = len(y)
     
@@ -179,16 +186,27 @@ def wls(x,y,u_y=None,fn=None):
     return WLSModel( coef,res,ssr,M,fn )
     
 #----------------------------------------------------------------------------
-def gls(x,y,cv,fn=None,label=None):
+def gls(x,y,cv=None,fn=None,label=None):
     """Generalised least squares fit of ``y`` to ``x``
     
-    :arg x: a sequence of ``M`` stimulus values (independent-variables)
-    :arg y: a sequence of ``M`` responses (dependent-variable)  
+    :arg x: an array of stimulus data (independent-variable)
+    :arg y: a 1-D array of response data (dependent-variable)
     :arg cv: an ``M`` by ``M`` real-valued covariance matrix for the responses
     :arg fn: a user-defined function relating ``x`` the response
     :returns:   an object containing regression results
-    :rtype:     :class:`GLSFit``
+    :rtype:     :class:`GLSModel``
+ 
+    The argument `x` may be an array of numbers or uncertain numbers. 
     
+    The argument `y` must be an array of uncertain numbers.
+
+    When the optional argument `cv` is not given, a variance-covariance 
+    matrix is obtained from the `y` elements.
+
+    When `fn` is provided, the routine will apply `fn` to each element 
+    in `x`, which must be a 1-D array, to obtain the rows of the objective matrix. 
+    When `fn` is `None`, `x` is taken as the objective matrix.
+ 
     """
     M = len(y)        
     if fn is None:
@@ -196,27 +214,38 @@ def gls(x,y,cv,fn=None,label=None):
     else:
         P = len( fn(x[0]) ) 
 
-    if cv.shape != (M,M):
-        raise RuntimeError( f"{cv.shape} should be {({M},{M})}" )     
+    if cv is None:
+        from GTC import (variance, get_covariance)
+        cv = np.empty( (M,M), dtype=np.float64 )
+        for i in range(M):
+            cv[i,i] = variance(y[i])
+            for j in range(i+1,M):
+                cv[i,j] = cv[j,i] = get_covariance(y[i],y[j])
+    else:
+        if cv.shape != (M,M):
+            raise RuntimeError( f"{cv.shape} should be {({M},{M})}" )     
     
-    # K = cholesky.cholesky_decomp(cv)
-    # Kinv = cholesky.cholesky_inv(K)
-    K = np.linalg.cholesky(cv)
-    Kinv = np.linalg.inv(K)
+    def row_values(row_i): 
+        return [ value(x_i) for x_i in row_i ]
         
     if fn is None:
-        X = np.array( x,dtype=object )
+        X = np.array( [ row_values(row_i) for row_i in x ],
+            dtype=np.float64 
+        )
     else:
         # fn expands the P basis function values at x_i
-        X = np.array( [ fn(x_i) for x_i in x ],
-            dtype=object
+        X = np.array( [ row_values(fn(x_i)) for x_i in x ],
+            dtype=np.float64
         )  
         
+    K = np.linalg.cholesky(cv)
+    Kinv = np.linalg.inv(K)
+
     Y = np.array( y, dtype=object ).T   
 
     a = Kinv @ X 
     b = Kinv @ Y 
-   
+
     coef = ols(a,b).beta
 
     res = np.array([ value(Y[i] - np.dot(coef,X[i])) for i in range(M) ]
